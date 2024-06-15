04b6dd7f4edc3a04c87d83e58be8cbb8d762511d
Charnjiv Brar <chruquantum@gmail.com>
2024-06-15 16:09:59 -0400

    use context example

---
+++ src/app/5-usecontext-example/page.tsx
@@ -0,0 +1,25 @@
+"use client"
+
+import { ThemeContext } from "@/context/ThemeContext"
+
+import React from "react";
+import { useContext} from "react";
+
+const UseContextExample = () => {
+   
+    const { state, dispatch } = useContext(ThemeContext)
+
+    console.log(state)
+
+    return (
+        <div className="useContextExample">
+            <button onClick={()=>dispatch({type:"CHANGE_THEME"})}>Change Theme</button>
+            <button onClick ={()=>dispatch({type:"CHANGE_FONTSIZE", payload:20})}>Change Font Size</button>
+        </div>
+
+    );
+
+
+};
+
+export default UseContextExample
