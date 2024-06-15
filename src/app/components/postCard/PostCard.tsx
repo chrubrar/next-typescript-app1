945e2b7025277d020b73cc39c7a6f9e02becc3a3
Charnjiv Brar <chruquantum@gmail.com>
2024-06-14 23:12:10 -0400

    second commit

--- src/app/components/postCard/PostCard.tsx
+++ src/app/components/postCard/PostCard.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 const PostCard = (props: {title:string; body:string}) => {
   return <div className="postCard">
-    <h1>{props.title}</h1>
-    <p>{props.body}</p>
+    <h1 className='text-zinc-500 text-2xl font-bold'>{props.title}</h1>
+    <h3 className="text-slate-500">{props.body}</h3>
     </div>
 }
 
