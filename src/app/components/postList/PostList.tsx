a8eb5c5ba85fea44cbee25483363635f1016e862
Charnjiv Brar <chruquantum@gmail.com>
2024-06-14 23:23:45 -0400

    third commit

--- src/app/components/postList/PostList.tsx
+++ src/app/components/postList/PostList.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import PostCard from '../postCard/PostCard'
+import { PostProps } from '../../types/types';
 
 async function getData() {
     const res = await fetch("https://jsonplaceholder.typicode.com/posts");
@@ -12,11 +13,11 @@ async function getData() {
 }
 
 const PostList = async () => {
-    const data = await getData()
+    const data:PostProps[] = await getData()
 
   return <div className='postList'>
     {/* <PostCard title="post1" desc="long post about react"/> */}
-    {data.map((post: {id:number; title: string; body: string}) => (
+    {data.map((post) => (
         <PostCard key={post.id} {...post} />
     ))}
   </div>
